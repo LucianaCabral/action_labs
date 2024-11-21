0f04a2bca924c8a4bcf0c12cc85aa5dcad403e16
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-11-21 08:36:20 -0300

    :white_check_mark: test [ACT-2] Add unit test

---
+++ test/features/Currencies/utils/exchange_rate_current_mock.dart
@@ -0,0 +1,5 @@
+import 'package:action_labs/features/currencies/domain/repository/exchange_rate_current_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class ExchangeRateCurrentMock extends Mock
+    implements ExchangeRateCurrentRepository {}
