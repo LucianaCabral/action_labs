7a2f8988d24faf57c4273f4eae1c9c402fe4540e
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-11-26 06:29:51 -0300

    :recycle: refactor [ACT-5] Refactor injection

--- lib/features/currencies/core/di/injection.dart
+++ lib/features/currencies/core/di/injection.dart
@@ -1,14 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
-import '../../currencies/data/mapper/current_exchange_rate_mapper.dart';
-import '../../currencies/data/mapper/current_exchange_rate_mapper_impl.dart';
-import '../../currencies/data/repository/current_exchange_rate_repository_impl.dart';
-import '../../currencies/data/source/current_exchange_rate_data_source.dart';
-import '../../currencies/data/source/current_exchange_rate_data_source_impl.dart';
-import '../../currencies/domain/repository/current_exchange_rate_repository.dart';
-import '../../currencies/domain/usecase/get_exchange_rate_current_use_case.dart';
-import '../network/ApiClient.dart';
+import '../../domain/repository/current_exchange_rate_repository.dart';
+import '../../domain/usecase/get_exchange_rate_current_use_case.dart';
+import '../../presentation/bloc/current_exchange_rate_bloc.dart';
+import '../api_client.dart';
+import '../data/mapper/current_exchange_rate_mapper.dart';
+import '../data/mapper/current_exchange_rate_mapper_impl.dart';
+import '../data/repository/current_exchange_rate_repository_impl.dart';
+import '../data/source/current_exchange_rate_data_source.dart';
+import '../data/source/current_exchange_rate_data_source_impl.dart';
 import '../network/service/exchange_rate_service.dart';
 
 final getIt = GetIt.instance;
@@ -38,4 +39,6 @@ Future<void> setupDependencies() async {
   getIt.registerLazySingleton<GetExchangeRateCurrentUseCase>(
     () => GetExchangeRateCurrentUseCase(getIt<CurrentExchangeRateRepository>()),
   );
+  getIt.registerFactory<CurrentExchangeRateBloc>(
+      () => CurrentExchangeRateBloc(getIt<GetExchangeRateCurrentUseCase>()));
 }
