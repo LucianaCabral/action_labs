fa1d2c4ecab6e45958e1da8d98c271d40661e21b
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-11-26 02:32:57 -0300

    :recycle: refactor:[ACT-3] Refactor unit test usecase domain layer

--- test/features/Currencies/domain/usecase/exchange_rate_current_use_case.dart_test.dart
+++ test/features/Currencies/domain/usecase/exchange_rate_current_use_case.dart_test.dart
@@ -1,5 +1,5 @@
-import 'package:action_labs/features/currencies/domain/repository/exchange_rate_current_repository.dart';
-import 'package:action_labs/features/currencies/domain/usecase/exchange_rate_current_use_case.dart';
+import 'package:action_labs/features/currencies/domain/repository/current_exchange_rate_repository.dart';
+import 'package:action_labs/features/currencies/domain/usecase/get_exchange_rate_current_use_case.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -7,20 +7,29 @@ import '../../utils/exchange_rate_current_mock.dart';
 import '../../utils/stubs.dart';
 
 void main() {
-  late ExchangeRateCurrentRepository exchangeRateCurrentRepository;
-  late ExchangeRateCurrentUseCase subject;
+  late CurrentExchangeRateRepository exchangeRateCurrentRepository;
+  late GetExchangeRateCurrentUseCase subject;
 
+  const fromSymbol = "USD";
+  const toSymbol = "BRL";
+
+  // Act
   setUpAll(() {
     exchangeRateCurrentRepository = ExchangeRateCurrentMock();
-    subject = ExchangeRateCurrentUseCase(exchangeRateCurrentRepository);
+    subject = GetExchangeRateCurrentUseCase(exchangeRateCurrentRepository);
   });
-  group("ExchangeRateCurrentUseCase: ", () {
+
+  // Arrange
+  group("GetExchangeRateCurrentUseCase: ", () {
     test('Should return a exchange rate current', () async {
-      when(() => exchangeRateCurrentRepository.getExchangeRateCurrent())
+      when(() => exchangeRateCurrentRepository.getExchangeRate(
+              fromSymbol: fromSymbol, toSymbol: toSymbol))
           .thenAnswer((_) => Future.value(exchangeRateCurrentMock));
 
-      final result = await subject.invoke();
+      final result = await subject.getExchangeRateCurrent(
+          fromSymbol: fromSymbol, toSymbol: toSymbol);
 
+      // Assert
       expect(result, exchangeRateCurrentMock);
     });
   });
--- test/features/Currencies/utils/stubs.dart
+++ test/features/Currencies/utils/stubs.dart
@@ -3,7 +3,7 @@ import 'package:action_labs/features/currencies/domain/entity/exchange_rate_curr
 final exchangeRateCurrentMock = ExchangeRateCurrent(
     exchangeRate: 0,
     fromSymbol: "fromSymbol",
-    lastPlaceAt: "2024-11-21T10:04:55.476Z",
-    rateLimitExceed: true,
+    lastUpdatedAt: "2024-11-21T10:04:55.476Z",
+    rateLimitExceeded: true,
     success: true,
     toSymbol: "toSymbol");
