18912ed5436dbdd0c5a541a25c7614dc0a818f44
LucianaCabral <eng.luciana.cabral@gmail.com>
2024-11-26 07:07:34 -0300

    :sparkles: feat [ACT-5] Added strings path

--- lib/features/currencies/presentation/resources/strings_path.dart
+++ lib/features/currencies/presentation/resources/strings_path.dart
@@ -1,3 +1,12 @@
 class StringPath {
   static const String errorMessage = 'Ops! Algo está errado';
+  static const String feedbackMessage = 'Opa! Este valor não é valor válido';
+  static const String dateFormat = 'dd/MM/yyyy - HH:mm';
+  static const String title = 'BRL Exchange Rate';
+  static const String labelText = 'Enter the currency code';
+  static const String headerResult = 'Exchange rate now';
+  static const String buttonResult = 'EXCHANGE RESULT';
+  static const String errorConnection = 'Opa, Tente novamente';
+  static const stringLocale = 'pt_BR';
 }
+
